dc8c3e74d4dfe8ac06cb002aa807604e9a1199ab
dnorton94 <dominiconorton@gmail.com>
2019-10-05 17:18:05 +0100

    heroku update

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+
+app.use(express.static('BAPRASHackathon'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/src/html/index.html')
 });
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
